6576f5298f7dfb84cae6741282e8b560a6fc051d
Willian da Silva <williandasilvaoficial@gmail.com>
2021-10-18 19:49:35 -0300

    add

--- Ex001.java
+++ Ex001.java
@@ -108,15 +108,19 @@ public class Ex001{
     }
     */
 
+    //Ex004
 
+    /*
     public class Ex001{
         public static void main(String[] args) {
             int x = 5, y = 6, z = 50;
             System.out.println(x + y + z);
         }
     }
+    */
 
 
+    
 
 
 
